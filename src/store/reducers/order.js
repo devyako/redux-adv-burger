8faf423c4d1d53a836c1440fcd65f11c8f407f02
devyako <yakhyoismoildjonov@gmail.com>
2020-04-15 12:28:43 +0500

    update

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -11,7 +11,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.PURCHASE_INIT:
       return {
         ...state,
-        purchased: false,
+        purchased: false, 
       };
 
     case actionTypes.PURCHASE_BURGER_SUCCESS:
